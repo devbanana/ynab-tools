9ce3064e7e969dcacdcee111571d04dc195670a0
Brandon Olivares <programmer2188@gmail.com>
2021-08-02 07:33:01 -0400

    Make primary key a uuid

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -29,6 +29,7 @@ use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Uid\Uuid;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -37,10 +38,9 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\Column(type="uuid")
      */
-    private $id;
+    private Uuid $id;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
@@ -58,7 +58,12 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $password;
 
-    public function getId(): ?int
+    public function __construct()
+    {
+        $this->id = Uuid::v1();
+    }
+
+    public function getId(): Uuid
     {
         return $this->id;
     }
