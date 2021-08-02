ea412c4d498ebe91f83a10e6660ac7bf24c52b7a
Brandon Olivares <programmer2188@gmail.com>
2021-08-01 20:17:22 -0400

    Update php-cs-fixer

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -12,7 +12,6 @@ return $config->setRules(array(
     'array_syntax' => array('syntax' => 'short'),
     'method_chaining_indentation' => true,
     'concat_space' => array('spacing' => 'one'),
-    'phpdoc_add_missing_param_annotation' => true,
     'phpdoc_line_span' => true,
     'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     'ordered_class_elements' => true,
