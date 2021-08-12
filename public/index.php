da31e051f2c5e114a09f122fe352d001cc29b736
Brandon Olivares <programmer2188@gmail.com>
2021-08-12 02:05:44 -0400

    Change directory structure and change root namespace

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Kernel;
+use Devbanana\YnabTools\Infrastructure\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
--- src/Infrastructure/Kernel.php
+++ src/Infrastructure/Kernel.php
@@ -23,7 +23,7 @@ declare(strict_types=1);
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-namespace App;
+namespace Devbanana\YnabTools\Infrastructure;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -36,26 +36,26 @@ final class Kernel extends BaseKernel
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
-        $container->import('../config/{packages}/*.yaml');
-        $container->import('../config/{packages}/' . $this->environment . '/*.yaml');
+        $container->import('../../config/{packages}/*.yaml');
+        $container->import('../../config/{packages}/' . $this->environment . '/*.yaml');
 
-        if (is_file(\dirname(__DIR__) . '/config/services.yaml')) {
-            $container->import('../config/services.yaml');
-            $container->import('../config/{services}_' . $this->environment . '.yaml');
+        if (is_file(\dirname(__DIR__, 2) . '/config/services.yaml')) {
+            $container->import('../../config/services.yaml');
+            $container->import('../../config/{services}_' . $this->environment . '.yaml');
         } else {
-            $container->import('../config/{services}.php');
+            $container->import('../../config/{services}.php');
         }
     }
 
     protected function configureRoutes(RoutingConfigurator $routes): void
     {
-        $routes->import('../config/{routes}/' . $this->environment . '/*.yaml');
-        $routes->import('../config/{routes}/*.yaml');
+        $routes->import('../../config/{routes}/' . $this->environment . '/*.yaml');
+        $routes->import('../../config/{routes}/*.yaml');
 
-        if (is_file(\dirname(__DIR__) . '/config/routes.yaml')) {
-            $routes->import('../config/routes.yaml');
+        if (is_file(\dirname(__DIR__, 2) . '/config/routes.yaml')) {
+            $routes->import('../../config/routes.yaml');
         } else {
-            $routes->import('../config/{routes}.php');
+            $routes->import('../../config/{routes}.php');
         }
     }
 }
