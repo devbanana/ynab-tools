3d4a70f6bfade393934f0aa3f9969630d97ab871
Brandon Olivares <programmer2188@gmail.com>
2021-08-14 12:40:18 -0400

    Add a few extra rules

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -1,9 +1,58 @@
 <?php
 
+declare(strict_types=1);
+
+/**
+ * This file is part of YNAB Tools.
+ *
+ * YNAB Tools is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * YNAB Tools is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with YNAB Tools.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author Brandon Olivares <programmer2188@gmail.com>
+ * @copyright (C) 2021 Brandon Olivares <programmer2188@gmail.com>
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $finder = PhpCsFixer\Finder::create()
     ->in('src')
     ->in('tests')
-    ->notName('bootstrap.php');
+    ->notName('bootstrap.php')
+    ->append([
+        __FILE__,
+        'rector.php',
+    ])
+;
+
+$header = <<<'EOF'
+    This file is part of YNAB Tools.
+
+    YNAB Tools is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    YNAB Tools is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with YNAB Tools.  If not, see <https://www.gnu.org/licenses/>.
+
+    @author Brandon Olivares <programmer2188@gmail.com>
+    @copyright (C) 2021 Brandon Olivares <programmer2188@gmail.com>
+    @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+    EOF;
 
 $config = new PhpCsFixer\Config();
 
@@ -15,6 +64,7 @@ return $config->setRules([
     'concat_space' => ['spacing' => 'one'],
     'align_multiline_comment' => ['comment_type' => 'all_multiline'],
     'phpdoc_line_span' => true,
+    'phpdoc_order' => true,
     'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
     'ordered_class_elements' => true,
     'explicit_indirect_variable' => true,
@@ -25,24 +75,7 @@ return $config->setRules([
         'heredoc_syntax' => true,
     ],
     'header_comment' => [
-        'header' => "This file is part of YNAB Tools.\n" .
-        "\n" .
-        "YNAB Tools is free software: you can redistribute it and/or modify\n" .
-        "it under the terms of the GNU General Public License as published by\n" .
-        "the Free Software Foundation, either version 3 of the License, or\n" .
-        "(at your option) any later version.\n" .
-        "\n" .
-        "YNAB Tools is distributed in the hope that it will be useful,\n" .
-        "but WITHOUT ANY WARRANTY; without even the implied warranty of\n" .
-        "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n" .
-        "GNU General Public License for more details.\n" .
-        "\n" .
-        "You should have received a copy of the GNU General Public License\n" .
-        "along with YNAB Tools.  If not, see <https://www.gnu.org/licenses/>.\n" .
-        "\n" .
-        "@author Brandon Olivares <programmer2188@gmail.com>\n" .
-        "@copyright (C) 2021 Brandon Olivares <programmer2188@gmail.com>\n" .
-        '@license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later',
+        'header' => $header,
         'comment_type' => 'PHPDoc',
         'location' => 'after_declare_strict',
         'separate' => 'both',
@@ -58,6 +91,7 @@ return $config->setRules([
     'final_class' => true,
     'final_public_method_for_abstract_class' => true,
     'date_time_immutable' => true,
+    'static_lambda' => true,
 ])
     ->setLineEnding("\n")
     ->setRiskyAllowed(true)
