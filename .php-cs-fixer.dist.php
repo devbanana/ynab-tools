9e0986588bd48eef328291425a5661ab1d817a6d
Brandon Olivares <programmer2188@gmail.com>
2021-08-22 01:46:00 -0400

    Install fixer configuration rules, plus perform package updates

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -23,7 +23,11 @@ declare(strict_types=1);
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$finder = PhpCsFixer\Finder::create()
+use Devbanana\FixerConfig\Configurator;
+use Devbanana\FixerConfig\PhpVersion;
+use PhpCsFixer\Finder;
+
+$finder = Finder::create()
     ->in('src')
     ->in('tests')
     ->notName('bootstrap.php')
@@ -54,64 +58,16 @@ $header = <<<'EOF'
     @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
     EOF;
 
-$config = new PhpCsFixer\Config();
-
-return $config->setRules([
-    '@PHP80Migration' => true,
-    '@Symfony' => true,
-    'array_indentation' => true,
-    'method_chaining_indentation' => true,
-    'concat_space' => ['spacing' => 'one'],
-    'align_multiline_comment' => ['comment_type' => 'all_multiline'],
-    'phpdoc_line_span' => true,
-    'phpdoc_order' => true,
-    'yoda_style' => ['equal' => false, 'identical' => false, 'less_and_greater' => false],
-    'ordered_class_elements' => [
-        'order' => [
-            'use_trait',
-            'constant_public',
-            'constant_protected',
-            'constant_private',
-            'property_public',
-            'property_protected',
-            'property_private',
-            'construct',
-            'destruct',
-            'method_static',
-            'magic',
-            'phpunit',
-            'method_public',
-            'method_protected',
-            'method_private',
+return Configurator::fromPhpVersion(PhpVersion::PHP_80())
+    ->withRiskyRulesEnabled()
+    ->withAddedRules([
+        'header_comment' => [
+            'header' => $header,
+            'comment_type' => 'PHPDoc',
+            'location' => 'after_declare_strict',
+            'separate' => 'both',
         ],
-    ],
-    'explicit_indirect_variable' => true,
-    'heredoc_to_nowdoc' => true,
-    'escape_implicit_backslashes' => [
-        'single_quoted' => true,
-        'double_quoted' => true,
-        'heredoc_syntax' => true,
-    ],
-    'header_comment' => [
-        'header' => $header,
-        'comment_type' => 'PHPDoc',
-        'location' => 'after_declare_strict',
-        'separate' => 'both',
-    ],
-    // Risky
-    '@PHP80Migration:risky' => true,
-    '@Symfony:risky' => true,
-    '@PHPUnit84Migration:risky' => true,
-    'php_unit_strict' => true,
-    'php_unit_test_case_static_method_calls' => ['call_type' => 'self'],
-    'strict_comparison' => true,
-    'strict_param' => true,
-    'final_class' => true,
-    'final_public_method_for_abstract_class' => true,
-    'date_time_immutable' => true,
-    'static_lambda' => true,
-])
-    ->setLineEnding("\n")
-    ->setRiskyAllowed(true)
+    ])
+    ->fixerConfig()
     ->setFinder($finder)
 ;
